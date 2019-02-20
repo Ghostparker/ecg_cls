b42d7ed882bb07a07c0cb8e25adb55830308d186
ghostparker <ghostparker@163.com>
2019-02-20 14:22:45 +0000

    add main function

--- train.py
+++ train.py
@@ -1,39 +1,47 @@
+import argparse
 import torch
 import torch.nn as nn
+
 from models.testmodel import TestModel
 from models.bulid_model import create_model
 from utils.MyDataset import MyDataset
-model_name = 'TestModel'
-#net = TestModel
-
-#######################
-'''cfg load'''
+from shutil import copyfile
 
 
-num_epoch = 50 
-device = 'cuda:1'
 
-model = create_model(model_name)
+def train():
+    model_name = 'TestModel'
 
-#from torch.utils import data.DataLoader
-##################################
-### train_loader 
-train_list_path = './all_list.txt'
-train_set = MyDataset(train_list_path)
-train_loader = torch.utils.data.DataLoader(dataset = train_set , batch_size = 10 , shuffle = False)
+    #
+    '''cfg load'''
 
-model = model.to(device)
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters() , lr = 1e-3)
-for epoch in range(num_epoch):
-    for idx , (images , labels) in enumerate(train_loader):
-        images = images.float().to(device)
-        labels = labels.long().to(device)
-        outputs = model(images)
-    
-        loss = criterion(outputs, labels)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        if(idx == 1):
-            print('epoch {} batch {}loss {}'.format(epoch , idx , loss.item()))
+
+    num_epoch = 44
+    device = 'cuda:1'
+
+    model = create_model(model_name)
+
+    train_list_path = './all_list.txt'
+    train_set = MyDataset(train_list_path)
+    train_loader = torch.utils.data.DataLoader(dataset = train_set , batch_size = 10 , shuffle = False)
+
+    model = model.to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters() , lr = 1e-3)
+    for epoch in range(num_epoch):
+        for idx , (images , labels) in enumerate(train_loader):
+            images = images.float().to(device)
+            labels = labels.long().to(device)
+            outputs = model(images)
+        
+            loss = criterion(outputs, labels)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if(idx == 1):
+                print('epoch {} batch {}loss {}'.format(epoch , idx , loss.item()))
+
+
+
+if __name__ == '__main__':
+    train()
