b266f41d0f40f9af2e22b9f9e9481a937c34d5ea
ghostparker <ghostparker@163.com>
2019-02-21 09:54:57 +0000

    edit the config 1

--- train.py
+++ train.py
@@ -4,7 +4,8 @@ import torch.nn as nn
 import os
 from models.testmodel import TestModel
 from models.bulid_model import create_model
-from utils.MyDataset import MyDataset
+from utils.MyDataset import MyDataset ,MyDataset1
+
 from shutil import copyfile
 
 
@@ -15,11 +16,20 @@ def train():
     '''cfg load'''
 
     '''parameters which will be optimized  '''
+
+    trainset_root = '/train/results/ecg_cls/'
+    train_images_set = (('list_index','all_list.txt'),('list_index','new_list.txt'))
     num_epoch = 44
-    device = 'cuda:1'
     is_pretrained = True
     load_model_path = './weight/f1.pth'
     save_model_dir = './weight'
+    use_gpu = True
+    gpu_id = 1
+
+    '''there is a problem about execute speed how to contrast device. and need to be optim'''
+    #device = 'cpu' if use_gpu else 'cuda:{}'.format(gpu_id)
+    device = 'cuda:1'
+    
     
     '''load the train model'''
     model = create_model(model_name)
@@ -29,10 +39,13 @@ def train():
         model.load_state_dict(torch.load(load_model_path))
         print('load pretrained end')
         
-     
+    ''' dataset load'''     
     train_list_path = './all_list.txt'
-    train_set = MyDataset(train_list_path)
+    #train_set = MyDataset(train_list_path)
+    train_set = MyDataset1(trainset_root,train_images_set)
     train_loader = torch.utils.data.DataLoader(dataset = train_set , batch_size = 10 , shuffle = True)
+    print(len(train_loader))
+
 
     model = model.to(device)
     criterion = nn.CrossEntropyLoss()
@@ -60,7 +73,7 @@ def save_checkpoint(model , path , name):
     if(os.path.exists(path)):
         torch.save(model.state_dict(),final_path)
         print('{} save end'.format(final_path))
-       
-    pass 
+    else:
+        print('file {} is not exist'.format(file_path))
 if __name__ == '__main__':
     train()
